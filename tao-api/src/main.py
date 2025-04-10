0344bf2dca57ca1036294a86cb7fb593991ace1d
Ryan <ryan@glorifiedstudios.com>
2025-04-10 19:44:01 +0200

    get_tao_dividends_per_subnet function

--- tao-api/src/main.py
+++ tao-api/src/main.py
@@ -1,44 +1,44 @@
-
 # Imports
 from fastapi import FastAPI
 from bittensor.core.chain_data import decode_account_id
 from bittensor.core.settings import SS58_FORMAT
 from async_substrate_interface import AsyncSubstrateInterface
 import asyncio
-import bittensor
+import time
 
 # Logic
-substrate = AsyncSubstrateInterface(
-    url="https://rpc.tao.network",
-    ss58_format=SS58_FORMAT,
-    type_registry_preset="tao"
-)
+async def get_tao_dividends_per_subnet():
+    async def exhaust(qmr):
+        r = []
+        async for k, v in await qmr:
+            r.append((k, v))
+        return r
 
-async def exhaust(qmr):
-    r = []
-    async for k, v in await qmr:
-        r.append((k, v))
-    return r
-
-async def fetch_last_dividend():
-    async with substrate:
-        block_hash = await substrate.get_block_hash()
+    start = time.time()
+    async with AsyncSubstrateInterface("wss://entrypoint-finney.opentensor.ai:443",
+                                       ss58_format=SS58_FORMAT) as substrate:
+        block_hash = await substrate.get_chain_head()
         tasks = [
             substrate.query_map(
                 "SubtensorModule",
                 "TaoDividendsPerSubnet",
                 [netuid],
-                block_hash=block_hash,
-            ) for netuid in range(1, 51)
+                block_hash=block_hash
+            ) for
+            netuid in range(1, 51)
         ]
         tasks = [exhaust(task) for task in tasks]
-
+        print(time.time() - start)
         results_dicts_list = []
         for future in asyncio.as_completed(tasks):
             result = await future
             results_dicts_list.extend([(decode_account_id(k), v.value) for k, v in result])
 
-        return results_dicts_list, block_hash
+    elapsed = time.time() - start
+    print(f"time elapsed: {elapsed}")
+
+    print("Async Results", len(results_dicts_list))
+    return results_dicts_list, block_hash
 
 # Routes
 app = FastAPI()
@@ -47,6 +47,7 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/dividend")
-async def dividend(netuid: int | None = None, hotkey: str | None = None, trade: bool = False):
-    return await fetch_last_dividend(netuid, hotkey, trade)
+@app.get("/api/v1/tao_dividends")
+async def get_dividends():
+    dividends = await get_tao_dividends_per_subnet()
+    return {"tao_dividends_per_subnet": dividends}
