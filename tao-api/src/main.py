bf7c9fb18e0a6bbb49ab533ff373e9543b73c59b
Ryan <ryan@glorifiedstudios.com>
2025-04-10 20:07:46 +0200

    filter for hotkey

--- tao-api/src/main.py
+++ tao-api/src/main.py
@@ -7,7 +7,10 @@ import asyncio
 import time
 
 # Logic
-async def get_tao_dividends_per_subnet(netuid: int):
+example_hotkey = "5FvFLiEWbDn5xdhjHt3M3E7ndjRYVr7ou8UjcvqhsMddxTJg"
+example_netuid = 1
+
+async def get_tao_dividends_per_subnet(netuid: int, hotkey: str):
     async with AsyncSubstrateInterface("wss://entrypoint-finney.opentensor.ai:443",
                                        ss58_format=SS58_FORMAT) as substrate:
         block_hash = await substrate.get_chain_head()
@@ -20,10 +23,13 @@ async def get_tao_dividends_per_subnet(netuid: int):
         
         results = []
         async for k, v in result:
-            results.append((decode_account_id(k), v.value))
+            decoded_key = decode_account_id(k)
+            if decoded_key == hotkey:
+                results.append((decoded_key, v.value)) # TODO: Check if there is a way we can query the hotkey directly instead of filtering it in the for loop.
+
         return results
 
-print(asyncio.run(get_tao_dividends_per_subnet(1)))
+print(asyncio.run(get_tao_dividends_per_subnet(example_netuid, example_hotkey)))
 
 # Routes
 app = FastAPI()
@@ -32,8 +38,7 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
-# TODO
-@app.get("/api/v1/tao_dividends")
-async def get_dividends():
-    dividends = await get_tao_dividends_per_subnet()
-    return {"tao_dividends_per_subnet": dividends}
+@app.get("/api/v1/tao_dividends/{netuid}/{hotkey}")
+async def get_dividends(netuid: int, hotkey: str):
+    dividends = await get_tao_dividends_per_subnet(netuid, hotkey)
+    return {"tao_dividends": dividends}
