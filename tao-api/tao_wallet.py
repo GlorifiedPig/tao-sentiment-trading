050145f434d0b77f536366f9c7cc338d50826a3a
Ryan <ryan@glorifiedstudios.com>
2025-04-16 19:52:55 +0200

    fix logic and add tests

--- tao-api/tao_wallet.py
+++ tao-api/tao_wallet.py
@@ -16,18 +16,20 @@ class TaoWallet:
         self.wallet = Wallet(name=WALLET_NAME, path=WALLET_PATH, hotkey=WALLET_HOTKEY)
         self.async_subtensor = async_subtensor.AsyncSubtensor(network=TESTNET_URL)
 
-    async def add_stake(self, netiud: int, amount: float):
-        return await self.async_subtensor.add_stake_extrinsic(
-            subtensor=self.wallet.subtensor,
+    async def add_stake(self, netiud: int, amount: float) -> bool:
+        return await self.async_subtensor.add_stake(
             wallet=self.wallet,
             netuid=netiud,
             amount=amount
         )
     
-    async def unstake(self, netiud: int, amount: float):
-        return await self.async_subtensor.unstake_extrinsic(
-            subtensor=self.wallet.subtensor,
+    async def unstake(self, netiud: int, amount: float) -> bool:
+        return await self.async_subtensor.unstake(
             wallet=self.wallet,
             netuid=netiud,
             amount=amount
         )
+    
+    async def test_stakes(self):
+        await self.add_stake(netiud=1, amount=0.01)
+        await self.unstake(netiud=1, amount=0.01)
