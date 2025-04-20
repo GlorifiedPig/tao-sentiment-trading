5f2ce42e64ae6ae461e3958aa2b7f85f04ad6696
Ryan <ryan@glorifiedstudios.com>
2025-04-20 15:50:33 +0200

    add extra logs for staking logic

--- tao-api/tao_wallet.py
+++ tao-api/tao_wallet.py
@@ -18,22 +18,35 @@ logging.setLevel("DEBUG")
 # Logic
 class TaoWallet:
     def __init__(self):
-        self.wallet = Wallet(name=WALLET_NAME, path=WALLET_PATH, hotkey=WALLET_HOTKEY)
-        #print("Generating new coldkey...")
-        self.wallet.create_new_coldkey(overwrite=True, use_password=False)
+        self.wallet = Wallet(name=WALLET_NAME, path=WALLET_PATH, hotkey=WALLET_HOTKEY) # NOTE: Make sure coldkey is not password protected!
+        print("Generating new coldkey...")
         self.async_subtensor = async_subtensor.AsyncSubtensor(network=TESTNET_URL)
 
     async def add_stake(self, netuid: int, amount: float) -> bool:
         try:
             subnet = await self.async_subtensor.subnet(netuid=netuid)
 
-            balance: Balance = tao(amount)
-            print(f"Subnet slippage for {netuid}: {subnet.slippage(balance)}")
+            block_hash = await self.async_subtensor.substrate.get_chain_head()
+
+            amount_balance: Balance = tao(amount)
+            print(f"Subnet slippage for {netuid}: {subnet.slippage(amount_balance)}")
+
+            wallet_balance = await self.async_subtensor.get_balance(self.wallet.coldkeypub.ss58_address)
+            print(f"Wallet balance: {wallet_balance}")
+
+            existential_deposit = await self.async_subtensor.get_existential_deposit(block_hash=block_hash)
+            print(f"Existential deposit: {existential_deposit}")
+
+            if amount_balance > wallet_balance - existential_deposit:
+                print(f"Not enough balance to stake {amount} on {netuid}")
+                return False
+
+            print(f"Staking {amount} on {netuid} with hotkey {subnet.owner_hotkey}")
 
             success: bool = await self.async_subtensor.add_stake(
                 wallet=self.wallet,
                 netuid=netuid,
-                amount=balance,
+                amount=amount_balance,
                 hotkey_ss58=subnet.owner_hotkey,
                 wait_for_inclusion=True,
                 wait_for_finalization=False,
@@ -73,5 +86,5 @@ class TaoWallet:
             return False
     
     async def test_stakes(self):
-        await self.add_stake(netuid=0, amount=0.1)
-        #await self.unstake(netuid=0, amount=0.02)
+        await self.add_stake(netuid=0, amount=0.03)
+        await self.unstake(netuid=0, amount=0.01)
